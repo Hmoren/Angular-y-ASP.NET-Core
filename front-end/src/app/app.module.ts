15bf6dea7cc0398d4ff4726ee7bb25191301f32b
Hernan Moreno <hernan.alexis.moreno@gmail.com>
2021-12-28 18:58:41 -0300

    31. Instalando Material Design

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ListadoPeliculasComponent } from './peliculas/listado-peliculas/listado-peliculas.component';
 import { ListadoGenericoComponent } from './utilidades/listado-generico/listado-generico.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MaterialModule } from './material/material.module'
 
 @NgModule({
   declarations: [
@@ -12,7 +15,9 @@ import { ListadoGenericoComponent } from './utilidades/listado-generico/listado-
     ListadoGenericoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
