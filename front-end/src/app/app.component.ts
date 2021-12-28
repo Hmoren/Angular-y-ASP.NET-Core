2a827d229e781652b487b0bad7a41d95ce54d64c
Hernan Moreno <hernan.alexis.moreno@gmail.com>
2021-12-28 16:03:26 -0300

    21. Interpolación - Mostrando Variables en el UI

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'front-end';
+  title = 'Al valor que yo quiera';
+
+  pelicula = {
+    titulo: 'Spider-Man',
+    fechaLanzamiento: new Date()
+  }
+
+  duplicarNumero(valor: number): number {
+    return valor * 2;
+  }
 }
