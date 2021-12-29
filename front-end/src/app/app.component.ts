b792687c482498e9439c64e8332b55dc02ec6ed3
Hernan Moreno <hernan.alexis.moreno@gmail.com>
2021-12-29 17:43:13 -0300

    40. Limpieza del Proyecto

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -11,12 +11,14 @@ export class AppComponent implements OnInit {
       this.peliculasEnCines = [{
         titulo: 'Spider-Man',
         fechaLanzamiento: new Date(),
-        precio: 1400.99
+        precio: 1400.99,
+        poster: 'https://m.media-amazon.com/images/M/MV5BMGZlNTY1ZWUtYTMzNC00ZjUyLWE0MjQtMTMxN2E3ODYxMWVmXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_FMjpg_UY720_.jpg'
       },
       {
         titulo: 'Moana',
         fechaLanzamiento: new Date('2016-11-14'),
-        precio: 300.99
+        precio: 300.99,
+        poster: 'https://m.media-amazon.com/images/M/MV5BMjI4MzU5NTExNF5BMl5BanBnXkFtZTgwNzY1MTEwMDI@._V1_FMjpg_UY576_.jpg 388w, https://m.media-amazon.com/images/M/MV5BMjI4MzU5NTExNF5BMl5BanBnXkFtZTgwNzY1MTEwMDI@._V1_FMjpg_UX1086_.jpg'
       }]
     },500)
     
