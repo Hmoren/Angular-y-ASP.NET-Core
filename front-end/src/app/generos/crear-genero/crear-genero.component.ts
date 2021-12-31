186c1fffc6ef48beea36edef97c30053d2fad05a
Hernan Moreno <hernan.alexis.moreno@gmail.com>
2021-12-31 12:11:17 -0300

    50. Validaciones Personalizadas

--- front-end/src/app/generos/crear-genero/crear-genero.component.ts
+++ front-end/src/app/generos/crear-genero/crear-genero.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { primeraLetraMayuscula } from 'src/app/utilidades/validadores/primeraLetraMayuscula';
 
 @Component({
   selector: 'app-crear-genero',
@@ -16,7 +17,7 @@ export class CrearGeneroComponent implements OnInit {
   ngOnInit(): void {
     this.form = this.formBuilder.group({
       nombre: ['', {
-        validators: [Validators.required, Validators.minLength(3)]
+        validators: [Validators.required, Validators.minLength(3), primeraLetraMayuscula()]
       }]
     })
   }
@@ -31,8 +32,12 @@ export class CrearGeneroComponent implements OnInit {
       return 'El campo nombre es requerido';
     }
 
-    if(campo.hasError('minLength')){
+    if(campo.hasError('minlength')){
       return 'La longitud minima des de 3 caracteres';
+    }
+
+    if(campo.hasError('primeraLetraMayuscula')){
+      return campo.getError('primeraLetraMayuscula').mensaje;
     }
 
     return '';
