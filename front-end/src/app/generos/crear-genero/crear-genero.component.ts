b9896228faab3ded19c506daee69c4fdacb37e3a
Hernan Moreno <hernan.alexis.moreno@gmail.com>
2021-12-31 11:04:01 -0300

    49. Validaciones en Formularios Reactivos

--- front-end/src/app/generos/crear-genero/crear-genero.component.ts
+++ front-end/src/app/generos/crear-genero/crear-genero.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -15,13 +15,27 @@ export class CrearGeneroComponent implements OnInit {
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
-      nombre: ''
+      nombre: ['', {
+        validators: [Validators.required, Validators.minLength(3)]
+      }]
     })
   }
 
   guardarCambios(){
     // ... guardar los cambios
     this.router.navigate(['/generos']);
+  }
+  obtenerErrorCampoNombre(){
+    var campo = this.form.get('nombre');
+    if(campo.hasError('required')){
+      return 'El campo nombre es requerido';
+    }
+
+    if(campo.hasError('minLength')){
+      return 'La longitud minima des de 3 caracteres';
+    }
+
+    return '';
   }
 
 }
