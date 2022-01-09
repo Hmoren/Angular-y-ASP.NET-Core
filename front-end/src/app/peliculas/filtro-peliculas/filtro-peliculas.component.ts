5649fd58390319a340b35966d73b7b9656210111
Hernan Moreno <hernan.alexis.moreno@gmail.com>
2022-01-08 22:39:12 -0300

    53. Filtro de Películas - Lógica

--- front-end/src/app/peliculas/filtro-peliculas/filtro-peliculas.component.ts
+++ front-end/src/app/peliculas/filtro-peliculas/filtro-peliculas.component.ts
@@ -18,16 +18,51 @@ export class FiltroPeliculasComponent implements OnInit {
     { id:3, nombre: 'Comedia'}
   ]
 
-  ngOnInit(): void {
-    this.form = this.formBuilder.group({
-      titulo: '',
-      generoId: 0,
-      proximosEstrenos:false,
-      enCines: false
-    });
+  peliculas = [
+    { titulo: 'Spider-Man: Fra From Home', enCines: false, proximosEstrenos: true,   generos: [1,2], poster: 'https://m.media-amazon.com/images/M/MV5BMGZlNTY1ZWUtYTMzNC00ZjUyLWE0MjQtMTMxN2E3ODYxMWVmXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_FMjpg_UY720_.jpg' },
+    { titulo: 'Moana',      enCines: true,  proximosEstrenos: false,  generos: [3],   poster: 'https://m.media-amazon.com/images/M/MV5BMjI4MzU5NTExNF5BMl5BanBnXkFtZTgwNzY1MTEwMDI@._V1_FMjpg_UX1086_.jpg' },
+    { titulo: 'Inception',  enCines: false, proximosEstrenos: false,  generos: [1,3], poster: 'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_FMjpg_UX700_.jpg' },
+  ]
+
+  peliculasOriginal = this.peliculas;
+
+  formularioOriginal = {
+    titulo: '',
+    generoId: 0,
+    proximosEstrenos:false,
+    enCines: false
   }
 
-  limpiar(){    
+  ngOnInit(): void {
+    this.form = this.formBuilder.group(this.formularioOriginal);
+
+    this.form.valueChanges.subscribe(valores =>{
+      this.peliculas = this.peliculasOriginal;
+      this.buscarPeliculas(valores);
+    })
+  }
+
+  buscarPeliculas(valores: any){
+    if(valores.titulo){
+      this.peliculas = this.peliculas.filter(pelicula => pelicula.titulo.indexOf(valores.titulo) !== -1);
+    }
+
+    if(valores.generoId !== 0){
+      this.peliculas = this.peliculas.filter(pelicula => pelicula.generos.indexOf(valores.generoId) !== -1);
+    }
+
+    if(valores.proximosEstrenos){
+      this.peliculas = this.peliculas.filter(pelicula => pelicula.proximosEstrenos);
+    }
+
+    if(valores.enCines){
+      this.peliculas = this.peliculas.filter(pelicula => pelicula.enCines);
+    }
+
+  }
+
+  limpiar(){  
+    this.form.patchValue(this.formularioOriginal);
   }
 
 }
